2fc09180d6f1c02a431e5641b2f83b1cc15d0938
Yuri Delgado <yuridelg@gmail.com>
2022-02-13 11:23:56 -0300

    More questions

--- src/components/ClickQuestion/questions.js
+++ src/components/ClickQuestion/questions.js
@@ -5,12 +5,51 @@ export const clickQuestions = {
         "can make beautiful big cakes. Everyone says they are delicious too.",
       correctAnswer: ["beautiful", "big", "delicious"],
     },
+    {
+      sentenceTail:
+        "bought a pair of new shoes. I saw them, and they are small and colorful.",
+      correctAnswer: ["new", "small", "colorful"],
+    },
+    {
+      sentenceTail:
+        "should apply for Medical School. My mom always says it is a great field of study.",
+      correctAnswer: ["great"],
+    },
+    {
+      sentenceTail: "is very loyal to my family.",
+      correctAnswer: ["loyal"],
+    },
+    {
+      sentenceTail:
+        "lives in a wonderful city. Most of the people who live there are polite and have a healthy life style.",
+      correctAnswer: ["wonderful", "polite", "healthy"],
+    },
   ],
   verbs: [
     {
       sentenceTail:
         "enjoy playing with children and old people. I think it is marvelous.",
       correctAnswer: ["enjoy", "playing", "think", "is"],
+    },
+    {
+      sentenceTail:
+        "bought a pair of new shoes. I saw them, and they are small and colorful.",
+      correctAnswer: ["bought", "saw", "are"],
+    },
+    {
+      sentenceTail:
+        "speaks English, Portuguese, Spanish, and is now learning French.",
+      correctAnswer: ["speaks", "is", "learning"],
+    },
+    {
+      sentenceTail:
+        "should apply for Medical School. My mom always says it is a great field of study.",
+      correctAnswer: ["apply", "says", "is"],
+    },
+    {
+      sentenceTail:
+        ", who is a famous person, likes to spend time with family.",
+      correctAnswer: ["is", "likes", "spend"],
     },
   ],
 };
